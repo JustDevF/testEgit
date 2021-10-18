8a900685109d7819f439811364fc6cb0d4687809
JUSTIN KATSI <justinkatasi.jk@gmail.com>
2021-10-18 09:31:20 +0200

    ajout des accesseurs

--- testEgit/src/testEgit/TestEgit.java
+++ testEgit/src/testEgit/TestEgit.java
@@ -8,6 +8,23 @@ public class TestEgit {
 		this.nom = nom;
 		this.nb = nb;
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getNb() {
+		return nb;
+	}
+
+	public void setNb(int nb) {
+		this.nb = nb;
+	}
+	
 	
 
 }
